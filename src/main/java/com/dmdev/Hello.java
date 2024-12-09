74d3811c3e470a335a4e3c29b7caffb880c74ea2
Pavel Glukhov <pvglukhov@gmail.com>
2024-12-09 23:07:52 +0300

    [master] transitive dependency

--- src/main/java/com/dmdev/Hello.java
+++ src/main/java/com/dmdev/Hello.java
@@ -2,6 +2,6 @@ package com.dmdev;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(StringUtils.capitilize("Hello World"));
     }
 }
