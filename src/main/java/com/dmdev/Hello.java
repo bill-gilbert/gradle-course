c65b87923a17b118a943118fe8485ccacf89a24d
Pavel Glukhov <pvglukhov@gmail.com>
2024-12-09 23:35:30 +0300

    [master] transitive dependency

--- src/main/java/com/dmdev/Hello.java
+++ src/main/java/com/dmdev/Hello.java
@@ -1,7 +1,10 @@
 package com.dmdev;
 
+import org.springframework.util.StringUtils;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println(StringUtils.capitilize("Hello World"));
+         System.out.println(StringUtils.capitalize("hello world"));
+        System.out.println("hELLO wORLD".toUpperCase());
     }
 }
